517cf0c983371f5b27b732648828f24ad76bc739
Tobias Nyholm <tobias.nyholm@gmail.com>
2014-07-28 12:38:59 +0200

    Fixed #16

--- src/Happyr/Doctrine/Specification/Spec.php
+++ src/Happyr/Doctrine/Specification/Spec.php
@@ -2,11 +2,20 @@
 
 namespace Happyr\Doctrine\Specification;
 
-use Happyr\Doctrine\Specification\Spec\AndX;
+use Happyr\Doctrine\Specification\Spec\AsArray;
 use Happyr\Doctrine\Specification\Spec\Comparison;
+use Happyr\Doctrine\Specification\Spec\In;
+use Happyr\Doctrine\Specification\Spec\IsNull;
 use Happyr\Doctrine\Specification\Spec\LogicX;
-use Happyr\Doctrine\Specification\Spec\OrX;
+use Happyr\Doctrine\Specification\Spec\Not;
+use Happyr\Doctrine\Specification\Spec\Specification;
 
+/**
+ * Factory class for the specifications
+ *
+ * @author Kacper Gunia
+ * @author Tobias Nyholm
+ */
 class Spec
 {
     public static function andX()
@@ -22,6 +31,31 @@ class Spec
     public static function collection()
     {
         return new LogicX(LogicX::AND_X, func_get_args());
+    }
+
+    public static function asArray(Specification $spec)
+    {
+        return new AsArray($spec);
+    }
+
+    public static function not(Specification $spec)
+    {
+        return new Not($spec);
+    }
+
+    public static function isNull($field, $value, $dqlAlias = null)
+    {
+        return new IsNull($field, $value, $dqlAlias);
+    }
+
+    public static function isNotNull($field, $value, $dqlAlias = null)
+    {
+        return new IsNotNull($field, $value, $dqlAlias);
+    }
+
+    public static function in($field, $value, $dqlAlias = null)
+    {
+        return new In($field, $value, $dqlAlias);
     }
 
     public static function eq($field, $value, $dqlAlias = null)
@@ -53,4 +87,4 @@ class Spec
     {
         return new Comparison(Comparison::GTE, $field, $value, $dqlAlias);
     }
-} 
+}
