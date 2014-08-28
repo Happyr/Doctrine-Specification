fccca3609f1769056ff666c1bb429e3c89017851
AndyBursh <andy.p.hunt91@gmail.com>
2014-08-28 20:37:41 +0100

    Adds default matching format for like

--- src/Happyr/Doctrine/Specification/Spec/Like.php
+++ src/Happyr/Doctrine/Specification/Spec/Like.php
@@ -8,7 +8,7 @@ class Like extends Comparison
     const ENDS_WITH = "%%%s";
     const STARTS_WITH = "%s%%";
 
-    public function __construct($field, $value, $format, $dqlAlias = null)
+    public function __construct($field, $value, $format = self::CONTAINS, $dqlAlias = null)
     {
         $formattedValue = $this->formatValue($format, $value);
         parent::__construct(self::LIKE, $field, $formattedValue, $dqlAlias);
