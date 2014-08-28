86fd95dca4eeab0c23a6a54ec7a536a59f00e0b2
AndyBursh <andy.p.hunt91@gmail.com>
2014-08-28 21:35:35 +0100

    Creates repository factory
    Provides implementation of the key
    \Doctrine\ORM\Repository\RepositoryFactory interface to make it easy for
    developers to use
    \Happyr\Doctrine\Specification\EntitySpecificationRepository as the
    default repository type

---
+++ src/Happyr/Doctrine/ORM/BaseEntitySpecificationRepository.php
@@ -0,0 +1,10 @@
+<?php
+namespace Happyr\Doctrine\ORM;
+
+
+class BaseEntitySpecificationRepository extends \Happyr\Doctrine\Specification\EntitySpecificationRepository
+{
+    // Implements nothing extra.
+    // Merely provides a concrete instance of \Happyr\Doctrine\Specification\EntitySpecificationRepository
+    // to use with \Happyr\Doctrime\ORM\EntitySpecificationRepositoryFactory
+} 
