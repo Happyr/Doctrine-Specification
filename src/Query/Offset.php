76811c3d8fe88b65633602c2a6e9873c8a0e258d
drewsonian <drewsonian@users.noreply.github.com>
2015-03-13 09:46:59 -0400

    Update Offset.php

--- src/Query/Offset.php
+++ src/Query/Offset.php
@@ -1,1 +1,25 @@
-
+<?php
+namespace Happyr\DoctrineSpecification\Query;
+use Doctrine\ORM\QueryBuilder;
+class Offset implements QueryModifier
+{
+    /**
+     * @var int limit
+     */
+    protected $offset;
+    /**
+     * @param int $limit
+     */
+    public function __construct($offset)
+    {
+        $this->offset = $offset;
+    }
+    /**
+     * @param QueryBuilder $qb
+     * @param string       $dqlAlias
+     */
+    public function modify(QueryBuilder $qb, $dqlAlias)
+    {
+        $qb->setFirstResult($this->offset);
+    }
+}
