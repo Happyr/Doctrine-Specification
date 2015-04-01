531ac99c2b19e6e5442c37f855c7490893d508f0
Drew Clauson <dclauson@elytus.com>
2015-04-01 15:15:48 -0400

    added notIn filter

--- src/Spec.php
+++ src/Spec.php
@@ -116,7 +116,12 @@ class Spec
     {
         return new In($field, $value, $dqlAlias);
     }
-
+	
+    public static function notIn($field, $value, $dqlAlias = null)
+    {
+        return new Not(new In($field, $value, $dqlAlias));
+    }
+	
     public static function eq($field, $value, $dqlAlias = null)
     {
         return new Comparison(Comparison::EQ, $field, $value, $dqlAlias);
