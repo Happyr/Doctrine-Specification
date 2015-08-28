fcea8c51c43ae2a8614e8d5c44731b8a3ace9aee
Diego Ferri <ferri.diego@gmail.com>
2015-08-28 11:01:58 +0200

    Specification Interface for Happyr/Doctrine-Specification#53

--- src/EntitySpecificationRepository.php
+++ src/EntitySpecificationRepository.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\EntityRepository;
 /**
  * This class allows you to use a Specification to query entities.
  */
-class EntitySpecificationRepository extends EntityRepository
+class EntitySpecificationRepository extends EntityRepository implements EntitySpecificationRepositoryInterface
 {
     use EntitySpecificationRepositoryTrait;
 }
