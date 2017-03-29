2688bceacb8eaf0ccfd8d50e30f5ccfcbe1ce1b0
Peter Gribanov <wcode404@gmail.com>
2017-03-29 17:58:26 +0300

    add annotations

--- src/Filter/Comparison.php
+++ src/Filter/Comparison.php
@@ -21,6 +21,9 @@ class Comparison implements Filter
     const GTE = '>=';
     const LIKE = 'LIKE';
 
+    /**
+     * @var array
+     */
     private static $operators = array(
         self::EQ, self::NEQ,
         self::LT, self::LTE,
@@ -42,6 +45,7 @@ class Comparison implements Filter
      * @var string dqlAlias
      */
     protected $dqlAlias;
+
     /**
      * @var string
      */
--- src/Filter/NotEquals.php
+++ src/Filter/NotEquals.php
@@ -4,6 +4,11 @@ namespace Happyr\DoctrineSpecification\Filter;
 
 class NotEquals extends Comparison
 {
+    /**
+     * @param string $field
+     * @param string $value
+     * @param string $dqlAlias
+     */
     public function __construct($field, $value, $dqlAlias = null)
     {
         parent::__construct(self::NEQ, $field, $value, $dqlAlias);
