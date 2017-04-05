0342af621d9fc743b2a922cdeafedcf17647d334
Peter Gribanov <wcode404@gmail.com>
2017-04-05 18:34:45 +0300

    create types map for register custom doctrine mapping type

--- src/DBALTypesResolver.php
+++ src/DBALTypesResolver.php
@@ -7,6 +7,14 @@ use Doctrine\DBAL\Types\Type;
 abstract class DBALTypesResolver
 {
     /**
+     * The map of supported doctrine mapping types.
+     *
+     * @var array
+     */
+    private static $_typesMap = array(
+    );
+
+    /**
      * Try get type for value.
      *
      * If type is not found return NULL.
@@ -18,8 +26,14 @@ abstract class DBALTypesResolver
     public static function tryGetTypeForValue($value)
     {
         if (is_object($value)) { // maybe it's a ValueObject
+            // try get type name from types map
+            $className = get_class($value);
+            if (isset(self::$_typesMap[$className])) {
+                return Type::getType(self::$_typesMap[$className]);
+            }
+
             // use class name as type name
-            $classNameParts = explode('\\', get_class($value));
+            $classNameParts = explode('\\', $className);
             $typeName = array_pop($classNameParts);
 
             if (array_key_exists($typeName, Type::getTypesMap())) {
@@ -28,5 +42,18 @@ abstract class DBALTypesResolver
         }
 
         return null;
+    }
+
+    /**
+     * Adds a custom type to the type map for resolve Value Object.
+     *
+     * @param string $name      The name of the type.
+     * @param string $className The class name of the Value Object.
+     *
+     * @return void
+     */
+    public static function addType($name, $className)
+    {
+        self::$_typesMap[$className] = $name;
     }
 }
