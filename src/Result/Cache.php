9cfe72bd8288384fa3946ea9a75c8659888d227d
Peter Gribanov <wcode404@gmail.com>
2017-04-07 12:13:25 +0300

    Round DateTime in query params

--- src/Result/Cache.php
+++ src/Result/Cache.php
@@ -3,6 +3,7 @@
 namespace Happyr\DoctrineSpecification\Result;
 
 use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\Query\Parameter;
 
 class Cache implements ResultModifier
 {
@@ -12,11 +13,18 @@ class Cache implements ResultModifier
     private $cacheLifetime;
 
     /**
-     * @param int $cacheLifetime How many seconds the cached entry is valid
+     * @var bool Round DateTime in query params
      */
-    public function __construct($cacheLifetime)
+    private $round;
+
+    /**
+     * @param int  $cacheLifetime How many seconds the cached entry is valid
+     * @param bool $round         Round DateTime in query params
+     */
+    public function __construct($cacheLifetime, $round = true)
     {
         $this->cacheLifetime = $cacheLifetime;
+        $this->round = $round;
     }
 
     /**
@@ -24,6 +32,20 @@ class Cache implements ResultModifier
      */
     public function modify(AbstractQuery $query)
     {
+        // round date params to cache lifetime
+        if ($this->round) {
+            foreach ($query->getParameters() as $parameter) {
+                /* @var $parameter Parameter */
+                if ($parameter->getValue() instanceof \DateTime) {
+                    // round down so that the results do not include data that should not be there.
+                    $date = clone $parameter->getValue();
+                    $date->setTimestamp(floor($date->getTimestamp() / $this->cacheLifetime) * $this->cacheLifetime);
+
+                    $query->setParameter($parameter->getName(), $date, $parameter->getType());
+                }
+            }
+        }
+
         $query->setResultCacheLifetime($this->cacheLifetime);
     }
 }
