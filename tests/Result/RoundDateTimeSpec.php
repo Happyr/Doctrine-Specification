6239a31b959f418ec2a7a9b6f467f61483e55a23
Peter Gribanov <wcode404@gmail.com>
2017-04-18 13:17:31 +0300

    Round DateTime in query params (#146)
    * Round DateTime in query params
    
    * add $round param in Spec::cache()
    
    * separate RoundTime result modifier
    
    * correct CS
    
    * rename RoundDateTime class
    
    * correct RoundDateTime class title
    
    * use \DateTime value as link
    
    * rename factory method for RoundDateTime spec
    
    * not use \DateTime in parameters as link
    
    * reset $date variable on change timestamp
    
    * test RoundDateTime specification
    
    * correct CS

---
+++ tests/Result/RoundDateTimeSpec.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace tests\Happyr\DoctrineSpecification\Result;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\Query\Parameter;
+use Happyr\DoctrineSpecification\Result\RoundDateTime;
+
+/**
+ * @mixin RoundDateTime
+ */
+class RoundDateTimeSpec
+{
+    private $roundSeconds = 3600;
+
+    public function let()
+    {
+        $this->beConstructedWith($this->roundSeconds);
+    }
+
+    public function it_is_a_specification()
+    {
+        $this->shouldBeAnInstanceOf('Happyr\DoctrineSpecification\Result\RoundDateTime');
+    }
+
+    public function it_round_date_time_in_query_parameters_for_given_time(
+        AbstractQuery $query,
+        Parameter $scalarParam,
+        Parameter $datetimeParam
+    ) {
+        $name = 'now';
+        $type = 'datetime';
+        $date = new \DateTime('15:55:34');
+
+        $scalarParam->getValue()->willReturn('foo');
+
+        $datetimeParam->getValue()->willReturn($date);
+        $datetimeParam->getName()->willReturn($name);
+        $datetimeParam->getType()->willReturn($type);
+
+        $query->getParameters()->willReturn(new ArrayCollection([$scalarParam, $datetimeParam]));
+        $query->setParameter($name, new \DateTime('15:00:00'), $type)->shouldBeCalled();
+
+        $this->modify($query);
+    }
+}
