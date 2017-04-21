8e0ef16065226c452a55c6423cee88c31856ebd3
Peter Gribanov <info@peter-gribanov.ru>
2017-04-21 22:11:42 +0300

    create AsSingleScalarTransformer

--- src/ResultModifier/AsSingleScalar.php
+++ src/ResultModifier/AsSingleScalar.php
@@ -1,20 +1,14 @@
 <?php
+/**
+ * Doctrine Specification.
+ *
+ * @author    Tobias Nyholm
+ * @copyright Copyright (c) 2014, Tobias Nyholm
+ * @license   http://opensource.org/licenses/MIT
+ */
 
 namespace Happyr\DoctrineSpecification\ResultModifier;
 
-use Doctrine\ORM\AbstractQuery;
-use Doctrine\ORM\Query;
-
-/**
- * Class AsSingleScalar.
- */
 class AsSingleScalar implements ResultModifier
 {
-    /**
-     * @param AbstractQuery $query
-     */
-    public function modify(AbstractQuery $query)
-    {
-        $query->setHydrationMode(Query::HYDRATE_SINGLE_SCALAR);
-    }
 }
---
+++ src/Transformer/Doctrine/ORM/Query/ResultModifier/AsSingleScalarTransformer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Doctrine Specification.
+ *
+ * @author    Tobias Nyholm
+ * @copyright Copyright (c) 2014, Tobias Nyholm
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace Happyr\DoctrineSpecification\Transformer\Doctrine\ORM\Query\ResultModifier;
+
+use Doctrine\ORM\AbstractQuery;
+use Happyr\DoctrineSpecification\ResultModifier\AsSingleScalar;
+use Happyr\DoctrineSpecification\ResultModifier\ResultModifier;
+use Happyr\DoctrineSpecification\Transformer\Doctrine\ORM\Query\QueryTransformer;
+
+class AsSingleScalarTransformer implements QueryTransformer
+{
+    /**
+     * @param ResultModifier $modifier
+     * @param AbstractQuery  $query
+     *
+     * @return AbstractQuery
+     */
+    public function transform(ResultModifier $modifier, AbstractQuery $query)
+    {
+        if ($modifier instanceof AsSingleScalar) {
+            $query->setHydrationMode(AbstractQuery::HYDRATE_SINGLE_SCALAR);
+        }
+
+        return $query;
+    }
+}
