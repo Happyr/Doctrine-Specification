0b4f223a734a2a624390b7fcc5d279e7ea18dace
Peter Gribanov <info@peter-gribanov.ru>
2017-04-21 23:20:19 +0300

    add file header comment

--- src/Exception/NonUniqueResultException.php
+++ src/Exception/NonUniqueResultException.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Doctrine Specification.
+ *
+ * @author    Tobias Nyholm
+ * @copyright Copyright (c) 2014, Tobias Nyholm
+ * @license   http://opensource.org/licenses/MIT
+ */
 
 namespace Happyr\DoctrineSpecification\Exception;
 
