0a2d78b576579e7f0a3cb3246039bf2d3cc40108
Peter Gribanov <info@peter-gribanov.ru>
2017-04-21 22:53:54 +0300

    create CountOfTransformer

--- src/QueryModifier/CountOf.php
+++ src/QueryModifier/CountOf.php
@@ -1,47 +1,16 @@
 <?php
+/**
+ * Doctrine Specification.
+ *
+ * @author    Tobias Nyholm
+ * @copyright Copyright (c) 2014, Tobias Nyholm
+ * @license   http://opensource.org/licenses/MIT
+ */
 
 namespace Happyr\DoctrineSpecification\QueryModifier;
 
-use Doctrine\ORM\QueryBuilder;
 use Happyr\DoctrineSpecification\Specification;
 
-/**
- * @author Tobias Nyholm
- */
 class CountOf implements Specification
 {
-    /**
-     * @var Specification child
-     */
-    private $child;
-
-    /**
-     * @param Specification $child
-     */
-    public function __construct(Specification $child)
-    {
-        $this->child = $child;
-    }
-
-    /**
-     * @param QueryBuilder $qb
-     * @param string       $dqlAlias
-     *
-     * @return string
-     */
-    public function getFilter(QueryBuilder $qb, $dqlAlias)
-    {
-        $qb->select(sprintf('COUNT(%s)', $dqlAlias));
-
-        return $this->child->getFilter($qb, $dqlAlias);
-    }
-
-    /**
-     * @param QueryBuilder $qb
-     * @param string       $dqlAlias
-     */
-    public function modify(QueryBuilder $qb, $dqlAlias)
-    {
-        $this->child->modify($qb, $dqlAlias);
-    }
 }
