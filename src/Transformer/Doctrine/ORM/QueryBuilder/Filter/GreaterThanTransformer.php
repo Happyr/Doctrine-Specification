b54c110b5155f929f5e45f773a4a852667b6bc82
Peter Gribanov <info@peter-gribanov.ru>
2017-04-22 14:59:18 +0300

    change file headers

--- src/Transformer/Doctrine/ORM/QueryBuilder/Filter/GreaterThanTransformer.php
+++ src/Transformer/Doctrine/ORM/QueryBuilder/Filter/GreaterThanTransformer.php
@@ -2,7 +2,7 @@
 /**
  * Doctrine Specification.
  *
- * @author    Tobias Nyholm
+ * @author    Tobias Nyholm <tobias@happyr.com>
  * @copyright Copyright (c) 2014, Tobias Nyholm
  * @license   http://opensource.org/licenses/MIT
  */
