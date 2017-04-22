1e6295c66b8048f5e69d6e317861f2ec750ff9fb
Peter Gribanov <info@peter-gribanov.ru>
2017-04-22 15:33:28 +0300

    create example MongoDB ODM transformers

---
+++ src/Transformer/Doctrine/ODM/MongoDB/QueryBuilder/ResultManagement/LimitTransformer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Doctrine Specification.
+ *
+ * @author    Tobias Nyholm <tobias@happyr.com>
+ * @copyright Copyright (c) 2014, Tobias Nyholm
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace Happyr\DoctrineSpecification\Transformer\Doctrine\ODM\MongoDB\QueryBuilder\ResultManagement;
+
+use Doctrine\ODM\MongoDB\Query\Builder;
+use Happyr\DoctrineSpecification\ResultManagement\Limit;
+use Happyr\DoctrineSpecification\Specification;
+use Happyr\DoctrineSpecification\Transformer\Doctrine\ODM\MongoDB\QueryBuilder\QueryBuilderTransformer;
+
+class LimitTransformer implements QueryBuilderTransformer
+{
+    /**
+     * @param Specification $specification
+     * @param Builder       $qb
+     *
+     * @return Builder
+     */
+    public function transform(Specification $specification, Builder $qb)
+    {
+        if ($specification instanceof Limit) {
+            $qb->limit($specification->getLimit());
+        }
+
+        return $qb;
+    }
+}
