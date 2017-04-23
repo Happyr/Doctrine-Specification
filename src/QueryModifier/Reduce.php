b462482a687ba0e18d2654da81d93eedd6f0ffac
Peter Gribanov <info@peter-gribanov.ru>
2017-04-23 18:37:26 +0300

    create Map/Reduce specifications and transformers for it

---
+++ src/QueryModifier/Reduce.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Doctrine Specification.
+ *
+ * @author    Tobias Nyholm <tobias@happyr.com>
+ * @copyright Copyright (c) 2014, Tobias Nyholm
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace Happyr\DoctrineSpecification\QueryModifier;
+
+interface Reduce extends QueryModifier
+{
+    /**
+     * @return string
+     */
+    public function getReduce();
+}
