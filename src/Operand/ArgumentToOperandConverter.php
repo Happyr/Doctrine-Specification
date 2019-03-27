059953ba63f4d049d8f5f562e6e1a76053cf61d6
Peter Gribanov <info@peter-gribanov.ru>
2019-03-27 22:36:44 +0300

    create ArgumentToOperandConverter

---
+++ src/Operand/ArgumentToOperandConverter.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Happyr\DoctrineSpecification\Operand;
+
+use Happyr\DoctrineSpecification\Exception\NotConvertibleException;
+
+class ArgumentToOperandConverter
+{
+    /**
+     * Convert the argument into the field operand if it is not an operand.
+     *
+     * @param Operand|string $argument
+     *
+     * @return Operand
+     */
+    public function convertField($argument)
+    {
+        if ($argument instanceof Operand) {
+            return $argument;
+        }
+
+        return new Field($argument);
+    }
+
+    /**
+     * Convert the argument into the value operand if it is not an operand.
+     *
+     * @param Operand|string $argument
+     *
+     * @return Operand
+     */
+    public function convertValue($argument)
+    {
+        if ($argument instanceof Operand) {
+            return $argument;
+        }
+
+        return new Value($argument);
+    }
+
+    /**
+     * Are all arguments is a operands?
+     *
+     * @param array $arguments
+     *
+     * @return bool
+     */
+    public function isAllOperands(array $arguments)
+    {
+        foreach ($arguments as $argument) {
+            if (!($argument instanceof Operand)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Convert all possible arguments to operands.
+     *
+     * @param Operand[]|string[] $arguments
+     *
+     * @throws NotConvertibleException
+     *
+     * @return Operand[]
+     */
+    public function convert(array $arguments)
+    {
+        if (!$arguments) {
+            return [];
+        }
+
+        // always try convert the first argument to the field operand
+        $field = $this->convertField(array_shift($arguments));
+
+        if (!$arguments) {
+            return [$field];
+        }
+
+        // always try convert the last argument to the value operand
+        $value = $this->convertValue(array_pop($arguments));
+
+        if (!$arguments) {
+            return [$field, $value];
+        }
+
+        if (!$this->isAllOperands($arguments)) {
+            throw new NotConvertibleException('You passed arguments not all of which are operands.');
+        }
+
+        array_unshift($arguments, $field);
+        array_push($arguments, $value);
+
+        return $arguments;
+    }
+}
