1bbe3bc0cbce66beb9e8834ee82d93216719b27a
Peter Gribanov <info@peter-gribanov.ru>
2019-03-28 23:03:19 +0300

    fix ComparisonSpec

--- tests/Filter/ComparisonSpec.php
+++ tests/Filter/ComparisonSpec.php
@@ -27,7 +27,7 @@ class ComparisonSpec extends ObjectBehavior
         $qb->getParameters()->willReturn($parameters);
         $parameters->count()->willReturn(10);
 
-        $qb->setParameter('comparison_10', 18)->shouldBeCalled();
+        $qb->setParameter('comparison_10', 18, null)->shouldBeCalled();
 
         $comparison = $this->getFilter($qb, null);
 
@@ -41,7 +41,7 @@ class ComparisonSpec extends ObjectBehavior
         $qb->getParameters()->willReturn($parameters);
         $parameters->count()->willReturn(10);
 
-        $qb->setParameter('comparison_10', 18)->shouldBeCalled();
+        $qb->setParameter('comparison_10', 18, null)->shouldBeCalled();
 
         $this->getFilter($qb, 'x')->shouldReturn('x.age > :comparison_10');
     }
