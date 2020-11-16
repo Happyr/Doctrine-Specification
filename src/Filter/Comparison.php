101315c172d28653093e1f53a4e15bb7b4275b14
Peter Gribanov <info@peter-gribanov.ru>
2020-11-16 17:09:21 +0300

    mark protected properties in Comparison class as private

--- src/Filter/Comparison.php
+++ src/Filter/Comparison.php
@@ -40,25 +40,19 @@ abstract class Comparison implements Filter
     const GTE = '>=';
 
     /**
-     * @deprecated This property will be marked as private in 2.0.
-     *
      * @var Operand|string
      */
-    protected $field;
+    private $field;
 
     /**
-     * @deprecated This property will be marked as private in 2.0.
-     *
      * @var Operand|string
      */
-    protected $value;
+    private $value;
 
     /**
-     * @deprecated This property will be marked as private in 2.0.
-     *
      * @var string|null
      */
-    protected $dqlAlias;
+    private $dqlAlias;
 
     /**
      * @var array
