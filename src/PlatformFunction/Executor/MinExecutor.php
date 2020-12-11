77e6b490377e104a3d808f539fe4f1a71563fd04
Peter Gribanov <info@peter-gribanov.ru>
2020-12-11 19:00:53 +0300

    create not executable AvgExecutor, CountExecutor, IdentityExecutor, MaxExecutor, MinExecutor, SumExecutor

---
+++ src/PlatformFunction/Executor/MinExecutor.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Karusel project.
+ *
+ * @copyright 2010-2020 АО «Карусель» <webmaster@karusel-tv.ru>
+ */
+
+namespace Happyr\DoctrineSpecification\PlatformFunction\Executor;
+
+use Happyr\DoctrineSpecification\Exception\OperandNotExecuteException;
+
+final class MinExecutor implements PlatformFunctionExecutor
+{
+    /**
+     * @param mixed ...$arguments
+     */
+    public function __invoke(...$arguments): void
+    {
+        throw new OperandNotExecuteException(
+            sprintf('Platform function "%s" cannot be executed for a single candidate.', __CLASS__)
+        );
+    }
+}
