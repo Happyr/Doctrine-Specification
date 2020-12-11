cae6e3045da7c8694b936d8952ef45387461ff75
Peter Gribanov <info@peter-gribanov.ru>
2020-12-11 19:04:22 +0300

    create DateDiffExecutor

---
+++ src/PlatformFunction/Executor/DateDiffExecutor.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Karusel project.
+ *
+ * @copyright 2010-2020 АО «Карусель» <webmaster@karusel-tv.ru>
+ */
+
+namespace Happyr\DoctrineSpecification\PlatformFunction\Executor;
+
+final class DateDiffExecutor implements PlatformFunctionExecutor
+{
+    /**
+     * @param \DateTimeInterface ...$arguments
+     *
+     * @return \DateInterval
+     */
+    public function __invoke(...$arguments): \DateInterval
+    {
+        return $arguments[0]->diff($arguments[1]);
+    }
+}
