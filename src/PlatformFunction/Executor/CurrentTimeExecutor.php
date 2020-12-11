9346709232a7cb4c2c79d0cf36c2a226837deb23
Peter Gribanov <info@peter-gribanov.ru>
2020-12-11 19:04:01 +0300

    create CurrentDateExecutor, CurrentTimeExecutor and CurrentTimestampExecutor

---
+++ src/PlatformFunction/Executor/CurrentTimeExecutor.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Karusel project.
+ *
+ * @copyright 2010-2020 АО «Карусель» <webmaster@karusel-tv.ru>
+ */
+
+namespace Happyr\DoctrineSpecification\PlatformFunction\Executor;
+
+final class CurrentTimeExecutor implements PlatformFunctionExecutor
+{
+    /**
+     * @param mixed ...$arguments
+     *
+     * @return \DateTimeImmutable
+     */
+    public function __invoke(...$arguments): \DateTimeImmutable
+    {
+        return (new \DateTimeImmutable())->setDate(1, 1, 1);
+    }
+}
