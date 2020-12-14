1a662fd715606a2dc192d74b6b4f31f9f57c8ff3
Peter Gribanov <info@peter-gribanov.ru>
2020-12-14 10:12:25 +0300

    need implement DateAddExecutor, DateSubExecutor and TrimExecutor

---
+++ src/PlatformFunction/Executor/DateAddExecutor.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Karusel project.
+ *
+ * @copyright 2010-2020 АО «Карусель» <webmaster@karusel-tv.ru>
+ */
+
+namespace Happyr\DoctrineSpecification\PlatformFunction\Executor;
+
+final class DateAddExecutor implements PlatformFunctionExecutor
+{
+    /**
+     * @param mixed ...$arguments
+     *
+     * @return mixed
+     */
+    public function __invoke(...$arguments)
+    {
+        // TODO need implement
+    }
+}
