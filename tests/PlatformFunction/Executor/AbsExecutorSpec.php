8aef8b138d8080139f2ac68fdabd95c17fff69f7
Peter Gribanov <info@peter-gribanov.ru>
2020-12-17 10:28:23 +0300

    test AbsExecutor

---
+++ tests/PlatformFunction/Executor/AbsExecutorSpec.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Karusel project.
+ *
+ * @copyright 2010-2020 АО «Карусель» <webmaster@karusel-tv.ru>
+ */
+
+namespace tests\Happyr\DoctrineSpecification\PlatformFunction\Executor;
+
+use Happyr\DoctrineSpecification\PlatformFunction\Executor\AbsExecutor;
+use PhpSpec\ObjectBehavior;
+
+/**
+ * @mixin AbsExecutor
+ */
+final class AbsExecutorSpec extends ObjectBehavior
+{
+    public function it_should_be_executable(): void
+    {
+        $this(-5)->shouldBe(5);
+        $this(5)->shouldBe(5);
+    }
+}
