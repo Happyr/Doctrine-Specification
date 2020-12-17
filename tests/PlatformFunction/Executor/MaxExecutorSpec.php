5948b8b5a57a90dee8409633e537296cf52ad093
Peter Gribanov <info@peter-gribanov.ru>
2020-12-17 10:53:13 +0300

    test MaxExecutor

---
+++ tests/PlatformFunction/Executor/MaxExecutorSpec.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of the Karusel project.
+ *
+ * @copyright 2010-2020 АО «Карусель» <webmaster@karusel-tv.ru>
+ */
+
+namespace tests\Happyr\DoctrineSpecification\PlatformFunction\Executor;
+
+use Happyr\DoctrineSpecification\Exception\OperandNotExecuteException;
+use Happyr\DoctrineSpecification\PlatformFunction\Executor\MaxExecutor;
+use PhpSpec\ObjectBehavior;
+
+/**
+ * @mixin MaxExecutor
+ */
+final class MaxExecutorSpec extends ObjectBehavior
+{
+    public function it_should_throw_exception_on_execute(): void
+    {
+        $this->shouldThrow(OperandNotExecuteException::class)->during('__invoke');
+    }
+}
